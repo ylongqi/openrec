f9c0a23712e4bd3629572d9a70be7012362b9dc1
ylongqi <ylongqi@gmail.com>
2018-01-07 22:20:02 -0500

    fix typo in download script

--- download_dataset.sh
+++ download_dataset.sh
@@ -18,11 +18,11 @@ mkdir -p dataset/amazon;
 wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/amazon/user_data_test.npy -O dataset/amazon/user_data_test.npy;
 wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/amazon/user_data_train.npy -O dataset/amazon/user_data_train.npy;
 wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/amazon/user_data_val.npy -O dataset/amazon/user_data_val.npy;
-wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/amazon/book_features_update.mem -O dataset/amazon/book_features_update.mem
-wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/amazon/user_features_categories.npy -O dataset/amazon/user_features_categories.npy
+wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/amazon/book_features_update.mem -O dataset/amazon/book_features_update.mem;
+wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/amazon/user_features_categories.npy -O dataset/amazon/user_features_categories.npy;
 
 mkdir -p dataset/netflix
 
-wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/netflix/netflix_ratings_formatted.npy -O dataset/netflix/netflix_ratings_formatted.npy
-wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/netflix/pretrained_item_embeddings.npy -O dataset/netflix/pretrained_item_embeddings.npy
-wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/netflix/pretrained_user_embeddings.npy -O dataset/netflix/pretrained_user_embeddings.npy
+wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/netflix/netflix_ratings_formatted.npy -O dataset/netflix/netflix_ratings_formatted.npy;
+wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/netflix/pretrained_item_embeddings.npy -O dataset/netflix/pretrained_item_embeddings.npy;
+wget https://s3.amazonaws.com/cornell-tech-sdl-openrec/netflix/pretrained_user_embeddings.npy -O dataset/netflix/pretrained_user_embeddings.npy;

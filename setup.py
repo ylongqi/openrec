5194590b8895e68c508049b44a8236f22403d6eb
Eugene <ebagdasaryan@me.com>
2018-02-04 09:23:04 -0500

    Updates for Python 3and switch to new version 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='openrec',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     description="An open-source and modular library for neural network-inspired recommendation algorithms",
     url="http://openrec.ai",
